c771322c769ebf0fdc7c41552a290e57f6fd1c00
CoconutNami <107953499+CoconutNami@users.noreply.github.com>
2023-02-07 14:55:59 -0600

    Initial commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.138.0.2:9200"
+	ES_USERNAME = "jing"
+	ES_PASSWORD = "891226"
+	GCS_BUCKET  = "around-bucket1"
+)
